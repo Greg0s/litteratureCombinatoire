44cdddc8e5576eda35156114ee4b28d7424f83f0
Greg0s <gregoire.tinnes@outlook.fr>
2023-01-13 20:34:26 +0100

    header almost good

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){  
 
-    $('.dropbtn, .secondBar, .dopbtn::before').hover(function() {
-        $('.secondBar').addClass('visible');
-    },
+  $('.dropbtn, .secondBar, .dopbtn::before').hover(function() {
+    if( $("body").innerWidth() > 930){
+      $('.secondBar').addClass('visible');
+    }
+  },
 
-    // mouse out
-    function() {
-        $('.secondBar').removeClass('visible');
-    });
+  // mouse out
+  function() {
+      $('.secondBar').removeClass('visible');
+  });
+
+  var sidenav = document.getElementById("mySidenav");
+  var openBtn = document.getElementById("openBtn");
+  var closeBtn = document.getElementById("closeBtn");
+
+  openBtn.onclick = openNav;
+  closeBtn.onclick = closeNav;
+
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    sidenav.classList.add("active");
+    $('.cross-icon').show();
+    $('.burger-icon').hide();
+  }
+
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    sidenav.classList.remove("active");
+    $('.cross-icon').hide();
+    $('.burger-icon').show();
+  }
 
 })
