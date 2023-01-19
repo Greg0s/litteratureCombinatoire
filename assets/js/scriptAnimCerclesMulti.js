d027c42fcf9ca98b8ab9edae61c4c1f587266cbe
Lucie Augier <lucieaugier28@gmail.com>
2023-01-19 13:30:23 +0100

    tests more circles

--- assets/js/scriptAnimCerclesMulti.js
+++ assets/js/scriptAnimCerclesMulti.js
@@ -7,7 +7,9 @@ var cercle3=document.querySelector('.cercle3');
 //var cercle2=document.querySelector('.cercle7');
 //var cercle2=document.querySelector('.cercle8');
 
-var colors = ['#FFD6A5', '#FDFFB6', '#CAFFBF', '#9BF6FF', '#A0C4FF', '#BDB2FF', '#FFADAD'];
+/*var colors = ['#FFD6A5', '#FDFFB6', '#CAFFBF', '#9BF6FF', '#A0C4FF', '#BDB2FF', '#FFADAD'];*/
+
+var colors = ['#03c03c', '#000000', '#cc0000', '#351c75', '#351c75', '#c90076']
 
 cercle1.style.background = colors[Math.floor(Math.random() * colors.length)];
 var color=cercle1.style.background;
@@ -15,10 +17,14 @@ cercle1.style["box-shadow"] = '0px 0px 100px 100px '+color;
 
 do{
     cercle2.style.background = colors[Math.floor(Math.random() * colors.length)];
-    var color2=cercle2.style.background;
+    var color2=cercle2.style.background;  
+}while(color2==color)
+
+do{
     cercle3.style.background = colors[Math.floor(Math.random() * colors.length)];
-    var color3=cercle3.style.background;    
-}while(color3==color2==color)
+    var color3=cercle3.style.background;  
+}while(color3==color2)
+
 
 cercle2.style["box-shadow"] = '0px 0px 100px 100px '+color2;
 cercle3.style["box-shadow"] = '0px 0px 100px 100px '+color3;
