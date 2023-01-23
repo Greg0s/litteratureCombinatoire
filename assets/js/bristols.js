e6747c9daed2a4f43cc5abe94b1c8fee86f0114b
Lucie Augier <lucieaugier28@gmail.com>
2023-01-23 22:27:28 +0100

    click on the button changes the text

---
+++ assets/js/bristols.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    var button = document.getElementById("button");
+    button.addEventListener("click", () => {
+        console.log("click");
+        document.getElementById("shuffle").innerHTML = "vers" ;
+    })
+
+})
+
