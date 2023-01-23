7955393ff66b663cce2dabe426a5f2f0bc401543
Lucie Augier <lucieaugier28@gmail.com>
2023-01-24 00:07:50 +0100

    tries failed

--- assets/js/bristols.js
+++ assets/js/bristols.js
@@ -1,10 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     var button = document.getElementById("button");
-    button.addEventListener("click", () => {
-        console.log("click");
-        document.getElementById("shuffle").innerHTML = "vers" ;
+    var bristols = ['vers1', 'vers2', 'vers3'];
+    bristols.push("c'est la fin");
+
+    var  nbIncrementations = 0; 
+ 
+
+    button.addEventListener("click", moveInTheArray(bristols, nbIncrementations));
+
     })
 
-})
+
+
+    function moveInTheArray(array, i)
+    {
+        i++; 
+        document.getElementById("shuffle").innerHTML = array[i];
+    }
+    
+
+    /*else {
+        console.log("fin");
+        document.getElementById("shuffle").innerHTML = "c'est la fin";
+    }*/
 
