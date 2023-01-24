b7702a8faf53448faf4ff2f70d905e28da423062
Lucie Augier <lucieaugier28@gmail.com>
2023-01-24 01:29:42 +0100

    not better

--- assets/js/bristols.js
+++ assets/js/bristols.js
@@ -1,42 +1,32 @@
 $(document).ready(function() {
 
-    var button = document.getElementById("button");
     var bristols = ['vers1', 'vers2', 'vers3'];
-    bristols.push("c'est la fin");
 
-    var  nbIncrementations = -1;
-    button.addEventListener("click", IncrementCompter(nbIncrementations));
+    var nbIncrementations = -1 ;
 
     if (nbIncrementations == -1)
     {
-        document.getElementById("shuffle").innerHTML = "Mélangez les bristols";
+        document.getElementById("newBristol").innerHTML = "Mélangez les bristols";
     }
 
     else if (nbIncrementations > -1 && nbIncrementations <= bristols.lenght)
     {
         while (nbIncrementations < bristols.lenght)
         {
-            document.getElementById("shuffle").innerHTML = bristols[nbIncrementations];
+            document.getElementById("newBristol").innerHTML = bristols[nbIncrementations];
         }
     }
 
     else 
     {
-        document.getElementById("shuffle").innerHTML = "Fin du jeu";
+        document.getElementById("newBristol").innerHTML = "Fin du jeu";
     }
 
     console.log(nbIncrementations);
 
 })
 
-function IncrementCompter (value)
-{
-    value = value + 1;
-}
-    
 
-    /*else {
-        console.log("fin");
-        document.getElementById("shuffle").innerHTML = "c'est la fin";
-    }*/
-
+function increase(){
+    nbIncrementations ++ ; 
+}    
