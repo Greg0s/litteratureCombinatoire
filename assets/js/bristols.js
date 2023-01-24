cfe459d7ada223541bed0782bba9608ef6017246
Lucie Augier <lucieaugier28@gmail.com>
2023-01-24 11:15:29 +0100

    idem

--- assets/js/bristols.js
+++ assets/js/bristols.js
@@ -1,25 +1,36 @@
 $(document).ready(function() {
 
-    var bristols = ['vers1', 'vers 2', 'vers 3'];
+  let bristols = ['vers1', 'vers 2', 'vers 3'];
+  
 
-// Select increment button
-const incrementCount = document.getElementById("button");
-const text = document.getElementById("text");
+  // Select increment button
+  const incrementCount = document.getElementById("button");
+  const text = document.getElementById("text");
 
-// Variable to track count
-var count = -1;
+  // Variable to track count
+  var count = -1;
+  var max = bristols.length;
+  
 
-// Display initial message
-text.innerHTML = "Mélangez les bristols" ;
+
+  // Display initial message
+  text.innerHTML = "Mélangez les bristols" ;
 
 
-// Function to increment count
-const handleIncrement = () => {
-  count++;
-  text.innerHTML = bristols[count];
-};
+  // Function to increment count
+  const handleIncrement = () => {
+    count++;
 
-// Add click event to buttons
-incrementCount.addEventListener("click", handleIncrement);
+    if (count >= max)
+    {
+      incrementCount.ariaDisabled = "true";
+      text.innerHTML = "Fin du jeu";
+    } 
+
+    text.innerHTML = bristols[count];
+  };
+
+  // Add click event to buttons
+  incrementCount.addEventListener("click", handleIncrement);
 
 })
